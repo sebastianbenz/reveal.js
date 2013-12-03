0be5c5dd4cc7e0a0419df2307f278cc087338bd2
Sebastian Benz <sebastian.benz@gmail.com>
2013-12-03 13:29:06 +0100

    add esr labs template

--- Gruntfile.js
+++ Gruntfile.js
@@ -48,7 +48,8 @@ module.exports = function(grunt) {
 					'css/theme/sky.css': 'css/theme/source/sky.scss',
 					'css/theme/moon.css': 'css/theme/source/moon.scss',
 					'css/theme/solarized.css': 'css/theme/source/solarized.scss',
-					'css/theme/blood.css': 'css/theme/source/blood.scss'
+					'css/theme/blood.css': 'css/theme/source/blood.scss',
+					'css/theme/esrlabs.css': 'css/theme/source/esrlabs.scss'
 				}
 			}
 		},
@@ -133,5 +134,6 @@ module.exports = function(grunt) {
 
 	// Run tests
 	grunt.registerTask( 'test', [ 'jshint', 'qunit' ] );
+  grunt.loadNpmTasks('grunt-notify');
 
 };
